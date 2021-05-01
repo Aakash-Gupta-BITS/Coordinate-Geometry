767ccd4ec6f750583f65f0abdf8ea70aa44de790
Aakash <f20180887@pilani.bits-pilani.ac.in>
2021-05-02 01:10:27 +0530

    Point class Added

---
+++ Structures/Helpers/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures.Helpers
+{
+    public class Point
+    {
+        public double X { get; set; } = 0;
+        public double Y { get; set; } = 0;
+
+        public double DistanceFromOrigin() => Math.Sqrt(X * X + Y * Y);
+
+        public override bool Equals(object obj) => obj is Point p && p.X == X && p.Y == Y;
+
+        public override int GetHashCode() => base.GetHashCode();
+
+        public override string ToString() => string.Format("({0}, {1})", X, Y);
+    }
+}
---
+++ Structures/Line.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Structures.Helpers;
+
+namespace Structures
+{
+    public class Line
+    {
+        public double A { get; private set; }
+        public double B { get; private set; }
+        public double C { get; private set; }
+
+        public Line(Point p1, Point p2)
+        {
+            if (p1.Equals(p2))
+                throw new ArgumentException(string.Format("Both points of input are same : {0}", p1));
+
+            A = p1.Y - p2.Y;
+            B = p2.X - p1.X;
+            C = -p1.Y * B - p1.X * A;
+        }
+    }
+}
